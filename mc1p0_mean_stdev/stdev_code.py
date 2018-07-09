1b595be06b56a0e208794018e6a708b810be05b0
Kishan Tailor <Tailorkishan123@gmail.com>
2018-07-08 22:30:48 -0400

    added sample and population std

--- mc1p0_mean_stdev/stdev_code.py
+++ mc1p0_mean_stdev/stdev_code.py
@@ -1,16 +1,23 @@
 import math as m
+import pandas as pd
+import numpy as np
+
 
 # calculate the population standard deviation
 def stdev_p(data):
     result = 2.0 # your code goes here
+    result = (np.array(data)).std()
     return result
+
 
 # calculate the sample standard deviation
 def stdev_s(data):
     result = 1.9 # your code goes here
+    result = (pd.Series(data)).std()
     return result
+
 
 if __name__ == "__main__":
     test = [2.0, 4.0, 4.0, 4.0, 5.0, 5.0, 7.0, 9.0]
-    print "the population stdev is", stdev_p(test)
-    print "the sample stdev is", stdev_s(test)
+    print ("the population stdev is", stdev_p(test))
+    print ("the sample stdev is", stdev_s(test))
